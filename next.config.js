0b2478cc090a9f204debca0aae46990d50ab45a1
Aryan <151847516+CodewithAryann@users.noreply.github.com>
2025-10-01 18:01:33 +0400

    1

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**", // adjust for your domain
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
